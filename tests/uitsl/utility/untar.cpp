25794214b2f861cf6e4d0a49ee681e459aa4e378
Matthew Andres Moreno <mmore500.login+git@gmail.com>
2020-09-30 15:39:24 -0400

    Add more untar tests

--- tests/uitsl/utility/untar.cpp
+++ tests/uitsl/utility/untar.cpp
@@ -4,10 +4,36 @@
 #include "uitsl/debug/compare_files.hpp"
 #include "uitsl/utility/untar.hpp"
 
-TEST_CASE("untar") {
+TEST_CASE("untar file") {
 
-  uitsl::untar( "assets/rick.tar" );
+  //REQUIRE(
+    uitsl::untar( "assets/rick.tar" );
+  //);
 
   REQUIRE( uitsl::compare_files( "assets/rick.txt", "rick.txt" ) );
 
 }
+
+TEST_CASE("untar nested directory") {
+
+  //REQUIRE(
+    uitsl::untar( "assets/nested.tar" );
+  //);
+
+  REQUIRE( uitsl::compare_files(
+    "assets/rick.txt", "never/gonna/let/you/down/rick.txt"
+  ) );
+
+}
+
+TEST_CASE("untar empty directory") {
+
+  //REQUIRE(
+    uitsl::untar( "assets/empty.tar" );
+  //);
+
+  REQUIRE( std::filesystem::exists( "empty/also_empty" ) );
+
+}
+
+// TODO longlink test
